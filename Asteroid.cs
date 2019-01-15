a32832fb4bd97414fa9cf3620b9b0183a211bd5c
FlyingToTheStars <46681924+FlyingToTheStars@users.noreply.github.com>
2019-01-15 07:30:03 +0100

    Add files via upload
    vorläufiges Script für die Asteroiden-Objekte

---
+++ Asteroid.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float speed;
+    public Transform player;
+    
+    void Start() {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * rotationSpeed;   // zufällige Rotation
+        //GetComponent<Rigidbody>().velocity = Random.insideUnitSphere * speed; // Bewegung mit fester Geschwindigkeit in zufällige Richtung
+        //GetComponent<Rigidbody>().velocity = Random.insideUnitSphere * Random.value * 10; // Bewegung mit zufälliger Geschwindigkeitsvektor in zufällige Richtung
+    }
+
+    void FixedUpdate() {
+        GetComponent<Rigidbody>().velocity = (player.position - GetComponent<Rigidbody>().position) *(speed/5f); // Spieler verfolgen, Geschwindigkeit anpassbar
+    }
+
+    // differenzierte Kollisionsabfragen, da sonst Objekte sofort wegen einer Kollision mit der Begrenzung zerstört werden -> Tags bei Objekten setzen
+
+    void OnTriggerEnter(Collider other) { // Kollisions mit anderen Objekten 
+        if (other.tag == "Shot") { 
+           // Instantiate(explosion, transform.position, transform.rotation); // Explosion erzeugen
+           Destroy(other.gameObject);
+           Destroy(gameObject);
+        } 
+        if (other.tag == "Player") {
+           //Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+           //gameController.GameOver();
+             Destroy(other.gameObject);
+             Destroy(gameObject);
+        }
+        if (other.tag == "Enemy") {
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity * -1f; // Abprallen des Asteroiden in Richtung, aus der er gekommen ist
+        }
+
+    void OnTriggerExit(Collider other) { // Verlassen des Spielraums
+        if (other.tag == "Boundary"){ // -> Begrenzungsobjekt muss entsprechenden Tag bekommen
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity *-1f; // Abprallen des Asteroiden in Richtung, aus der er gekommen ist -> Level basieren auf Asteroidenanzahl
+            //Destroy(gameObject); // Zerstören des Asteroiden -> Level sind zeitbasiert
+        }
+    }
+}
