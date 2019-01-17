fd90fea14439f980ca4846135c7d24733428ad85
EyDude <43246023+EyDude@users.noreply.github.com>
2019-01-17 21:59:51 +0100

    Add files via upload

---
+++ AsteroidShooter.cs
@@ -0,0 +1,21 @@
+﻿// Unterklasse der Klasse Asteroid
+// für Asteroiden, die auf den Spieler schießen
+// bewegen sich ansonsten mit konstanter Geschwindigkeit
+
+using UnityEngine;
+
+public class AsteroidShooter : Asteroid {
+
+    public int frequency;
+
+    new void Start()
+    {
+        base.Start();
+        rb.velocity = Random.insideUnitSphere * speed * Time.deltaTime; // Bewegung mit fester Geschwindigkeit in zufällige Richtung
+    }
+
+    void Update()
+    {
+       // Schuss nach bestimmter Zeit abfeuern 
+    }
+}
